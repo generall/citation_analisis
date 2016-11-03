99178f642c41b42335ace6e6bb923758aea52921
Mikhail <panteleevmf@gmail.com>
2016-11-03 18:53:54 +0300

    dafsgdf

--- network.py
+++ network.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
@@ -37,6 +39,7 @@ class CoauthorNetwork:
         self.coauth_count = {}
         self.coauth_year = {}
         self.author_year = {}
+        self.auth_cited_by_year = {}
         
     def add_article(self, article):
         article_id = article.paper_index
@@ -55,7 +58,6 @@ class CoauthorNetwork:
             self.coauth_count[pair] = self.coauth_count.get(pair, 0) + 1
             self.coauth_year[pair] = self.coauth_year.get(pair, []) + [article.year]
             self.gr.add_edge(pair[0], pair[1])
-
     
     def get_articles_by_author(author):
         [self.articles.get(idx) for idx in author_to_article.get(author, [])]
@@ -71,12 +73,21 @@ class CoauthorNetwork:
             for author in article.authors:
                 author = author.strip()
                 self.cgr.add_node(author)                
-            for cited_article in article.references_ids:
-                if self.articles.get(cited_article) != None:
-                    for cited_author in self.articles.get(cited_article).authors:
+            for cited_article_id in article.references_ids:
+                cited_article = self.articles.get(cited_article_id)
+                if cited_article != None:
+                    for cited_author in cited_article.authors:
                         cited_author = cited_author.strip()                    
                         self.cgr.add_node(cited_author) 
                         self.cgr.add_edge(author, cited_author)
+                        pair = (cited_author, author)
+                        # self.auth_cited_by_year[pair] = self.auth_cited_by_year.get(pair, []) + [article.year]
+
+                        year_number_dict =  self.auth_cited_by_year.get(author, {})
+                        year_number_dict[article.year] = year_number_dict.get(article.year, 0) + 1
+                        self.auth_cited_by_year[author] = year_number_dict
+
+
 
 
     def load_with_loader(file, loader, article_filter = None):
@@ -136,10 +147,8 @@ class CoauthorNetwork:
             coauthor_distribution[len(article.authors)] = coauthor_distribution.get(len(article.authors), 0) + 1
 
         for cite_node in self.cgr.nodes():
-            if len(in_nodes) != 0:
-                cite_distribution[self.cgr.in_degree(cite_node)] = cite_distribution.get(self.cgr.in_degree(cite_node)), 0) + 1
-            else:
-                cite_distribution[0] = cite_distribution.get(0, 0) + 1
+            cite_distribution[self.cgr.in_degree(cite_node)] = cite_distribution.get(self.cgr.in_degree(cite_node), 0) + 1
+
 
         coauthor_distribution = sorted(coauthor_distribution.items(), key=operator.itemgetter(0))
         cite_distribution = sorted(cite_distribution.items(), key=operator.itemgetter(0))
@@ -147,6 +156,41 @@ class CoauthorNetwork:
         print("coauth:", coauthor_distribution[:10])
         print("cite:", cite_distribution[:10])
         return (coauthor_distribution, cite_distribution)
+
+    def top_authors_cite_coauth_stat(self):
+        STEP = 3
+        author_year_coauthors_num = {}
+        author_year_cites_num = {}
+            
+        auth_coauth_pagerank = calc_pagerank(self.gr, 10)[2]
+
+        author_year_coauthors_num = dict( (author, Counter(years) ) for author, years in self.author_year.items() )
+
+        return [author_year_coauthors_num.get(author, {}) for author, weight in auth_coauth_pagerank ]
+
+
+
+        # filtered_year_coauth = [for author, years_dict in author_year_coauthors_num.items()]
+
+        # for author, weight in auth_coauth_pagerank:
+        #     for coauthor in self.gr.neighbors(author):
+        #         for year in self.coauth_year.get((author, coauthor), []):
+        #             year_number_dict = author_year_coauthors_num.get(author, {})
+        #             year_number_dict[year] = year_number_dict.get(year, 0) + 1
+        #             author_year_coauthors_num[author] = year_number_dict
+
+        # auth_cite_pagerank = calc_pagerank(self.cgr, 10)[2]
+        # for author, weight in auth_cite_pagerank:
+        #     for who_cited in self.cgr.in_edges(author):
+        #         for year in self.auth_cited_by_year.get((author, who_cited), []):
+        #             year_number_dict =  articles_cited_in_year.get(author, {})
+        #             year_number_dict[year] = year_number_dict.get(year, 0) + 1
+        #             author_year_cites_num[author] = year_number_dict
+
+        # return (author_year_coauthors_num, self.auth_cited_by_year)
+
+
+
 
 
     
